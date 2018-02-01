5d780079047f893213dd1c2a104da0b61fa2e563
LauraDami <cae.la.noche@gmail.com>
2018-02-01 11:11:37 +0100

    he puesto un comentario

---
+++ IE_331/src/dam/obj/CD.java
@@ -0,0 +1,47 @@
+package dam.obj;
+
+
+public class CD {
+
+		private String nombre;
+		private int ano;
+		private int pistas;
+		private Estilos estilo;
+	
+		CD (String nombre, int ano, String estilo, int pistas) {
+			this.nombre=nombre;
+			nombre.toLowerCase();
+			this.ano=ano;
+			this.estilo=Estilos.getEstilo(estilo);
+			this.pistas=pistas;
+		}
+		//comentario
+		@Override
+		public boolean equals (Object obj) {
+			if (obj==null) {return false;}
+			if (getClass()!=obj.getClass()) {return false;}
+			final CD other=(CD)obj;
+			if (this.nombre!=other.nombre) {return false;}
+			return true;
+		}
+		
+		public String getNombre() {
+			return nombre;
+		}
+		
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		
+		public int getano() {
+			return ano;
+		}
+		
+		public void setano(int ano) {
+			this.ano = ano;
+		}
+		
+		public String toString () {
+			return "\n"+nombre+" en el "+ano+", tiene "+pistas+" pistas y el estilo es "+estilo;
+		}
+	}
